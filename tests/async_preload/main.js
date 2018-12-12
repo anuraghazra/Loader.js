d00ce58ead89acca6d8370672d08f8d62f46f875
anuraghazra <hazru.anurag@gmail.com>
2018-12-12 15:52:27 +0530

    manual test for loading with canvas

---
+++ tests/async_preload/main.js
@@ -0,0 +1,33 @@
+let canvas = document.getElementById('c');
+let width = canvas.width = 500;
+let height = canvas.height = 500;
+let c = canvas.getContext('2d');
+
+
+const loader = new LoaderJS();
+
+loader.loadImage('one', 'https://cdn2.scratch.mit.edu/get_image/user/9706068_60x60.png');
+loader.loadImage('two', 'https://media.giphy.com/media/f4qECMR9aozPq/giphy.gif');
+
+
+loader.whenReady = function() {
+  // do something
+}
+
+let x = 20;
+function animate() {
+  c.clearRect(0, 0, width, height);
+
+  if(loader.loaded) {
+    c.drawImage(loader.assets.one, 200, 100, 100, 100);
+    c.drawImage(loader.assets.two, 0, 100, 100, 100);
+  } else {
+    c.fillText('loading img', 20, 20)
+  }
+
+  c.fillStyle = 'red';
+  c.fillRect(x, 100, 20, 20)
+  x++;
+  requestAnimationFrame(animate)
+}
+animate();
